6bf9593fb868fa7ca385b8139ba57fc2b2d1586b
shadab <me.shadab19@gmail.com>
2022-09-19 17:40:27 +0530

    [Added:~ FullTime Salary Logic]

--- src/com/Emp/EmpWageProblem.java
+++ src/com/Emp/EmpWageProblem.java
@@ -7,12 +7,17 @@ public class EmpWageProblem {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int randomNum = random.nextInt(2);
 
-		if (randomNum == 1)
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int salary = 0;
+		int randomNum = random.nextInt(2);
+		if (randomNum == 1) {
 			System.out.println("Employee  is Present");
-		else
+			salary = wagePerHour * fullDayHours;
+		} else
 			System.out.println("Employee  is Absent");
+		System.out.println("salary is: "+salary);
 
 	}
 
