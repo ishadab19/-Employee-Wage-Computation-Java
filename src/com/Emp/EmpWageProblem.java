1d06b9c0c3986825c528ed714fcc28d40821d097
shadab <me.shadab19@gmail.com>
2022-09-19 17:12:06 +0530

    [Added:~ Present Absent Logic]

--- src/com/Emp/EmpWageProblem.java
+++ src/com/Emp/EmpWageProblem.java
@@ -1,8 +1,19 @@
 package com.Emp;
 
+import java.util.Random;
+
 public class EmpWageProblem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+		Random random = new Random();
+		int randomNum = random.nextInt(2);
+
+		if (randomNum == 1)
+			System.out.println("Employee  is Present");
+		else
+			System.out.println("Employee  is Absent");
+
 	}
 
 }
